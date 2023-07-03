6f65bbd1d63d6c0a8f881b3e40973c6a52eee816
MAITAYORMARIS <yormarism@gmail.com>
2023-07-02 21:29:49 -0300

    ajustando datos del json

--- models/citiesmodel.js
+++ models/citiesmodel.js
@@ -4,10 +4,15 @@ const citiesSchema = new mongoose.Schema({
     name:{type:String, required:true},
     banner:{type:String,required:true},
     country:{type:String,required:true},
+   language:{type:String},
     image:{type:String,required:true},
     info:{type:String,required:true},
     phrase:{type:String,required:true},
-    region:{type:String,required:true}
+    region:{type:String,required:true},
+    galUno:{type:String},
+    galDos:{type:String},
+    galTres:{type:String}
+
 })
 
 // nueva constante que usa el modelado mongoose para crear la coleccion cities segun el modelo citiesSchema
