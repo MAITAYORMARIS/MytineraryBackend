189b89364a7ffccc403725608c1ca4a273f627f8
MAITAYORMARIS <yormarism@gmail.com>
2023-07-03 21:27:12 -0300

    ajustando cities model

--- models/citiesmodel.js
+++ models/citiesmodel.js
@@ -4,14 +4,14 @@ const citiesSchema = new mongoose.Schema({
     name:{type:String, required:true},
     banner:{type:String,required:true},
     country:{type:String,required:true},
-   language:{type:String},
+   language:{type:String, required:true},
     image:{type:String,required:true},
     info:{type:String,required:true},
     phrase:{type:String,required:true},
     region:{type:String,required:true},
-    galUno:{type:String},
-    galDos:{type:String},
-    galTres:{type:String}
+    galUno:{type:String,required:true},
+    galDos:{type:String,required:true},
+    galTres:{type:String,required:true}
 
 })
 
